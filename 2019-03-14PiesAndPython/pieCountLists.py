2d827d0ae08d54b77a38c8e48fc4959ce499a659
Eric Hanson <ehanson@mit.edu>
2019-03-05 14:36:01 -0500

    adding scripts for presentation

---
+++ 2019-03-14PiesAndPython/pieCountLists.py
@@ -0,0 +1,36 @@
+import csv
+
+pieListFull = []
+pieListFullEdited = []
+pieListUnique = []
+
+#create full list of values from spreadsheet
+with open('input.csv','r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        pie = row[1]
+        pieListFull.append(pie)
+print(pieListFull)
+
+#normalize data and create list of edited values
+for pie in pieListFull:
+    editedPie = pie.lower()
+    editedPie= editedPie.replace(' pie', '')
+    pieListFullEdited.append(editedPie)
+
+#write csv with a header row
+f=csv.writer(open('pieReportLists.csv', 'w'))
+f.writerow(['pie']+['count'])    
+
+#create list of unique values
+for editedPie in pieListFullEdited:
+    if editedPie not in pieListUnique:
+        pieListUnique.append(editedPie)
+    else:
+        pass
+print(pieListUnique)
+
+#write csv content
+for pie in pieListUnique:
+    pieCount = pieListFullEdited.count(pie)
+    f.writerow([pie]+[pieCount])
